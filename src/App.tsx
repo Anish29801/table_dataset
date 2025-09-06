21dc548e9c4ed3e915ad30b10ecabab1bc3b52ad
Anish29801 <edu.anish29801@gmail.com>
2025-09-06 12:38:58 +0530

    Intial Project Commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+function App() {
+  return (
+    <div>
+      <Typography variant="h4">Hello World</Typography>
+    </div>
+  );
+}
+
+export default App;
